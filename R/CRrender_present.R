74e75e7c33b5f54c763dfbf333b1f2b9566ff6fb
lorweiuk <33628864+lorweiuk@users.noreply.github.com>
2018-03-01 14:01:16 +0100

    Update CRrender_present.R

--- R/CRrender_present.R
+++ R/CRrender_present.R
@@ -5,23 +5,32 @@
 #' param graphics_list a list created with CRopen(), containing pointers to window, renderer, surface and event
 #' return return times in milliseconds when the call to present returned; this can be used as best estimate when graphical information appeared on screen
 
-CRrender_present <- function(graphics_list, wait = FALSE) {
+CRrender_present <- function(graphics_list, wait = FALSE, vsync = FALSE) {
 
   if ( !isGraphicsList(graphics_list) ) {
     stop("Input structure must be \"graphics_list\".")
   }
   if ( !is.logical(wait) ) {
-    stop("Input \"wait\" must be TRUE or FALSE");
+    stop("Input \"wait\" must be TRUE or FALSE")
+  }
+  if ( !is.logical(vsync) ) {
+    stop("Input \"wait\" must be TRUE or FALSE")
   }
     
   if (wait) {
-      wait = 1;
+      wait = 1
   } else {
-      wait = 0;
+      wait = 0
+  }
+  
+  if (vsync) {
+      vsync = 1
+  } else {
+      vsync = 0
   }
   
   graphics_list = orderGraphicsList(graphics_list)
-  out <- .Call("SDLrender_present", graphics_list, wait, 0, PACKAGE = "SDLDLL")
+  out <- .Call("SDLrender_present", graphics_list, wait, vsync, PACKAGE = "SDLDLL")
 
   return(out)
   

bffe391a5b4d171b7327a3b5eea0900e5ac1e814
lorweiuk <33628864+lorweiuk@users.noreply.github.com>
2020-10-07 08:34:14 +0200

    Add files via upload

--- R/CRcolor_to_transparent.R
+++ R/CRcolor_to_transparent.R
@@ -1,16 +1,24 @@
-#' Set color in color_list to transparent for the surface pointed to by surfacep
+#' Set color in color_list to transparent for the image pointed to by image_list and index
 #'
-#' Set color in color_list to transparent for the surface pointed to by surfacep
-#' surfacep is usually an image loaded with CRload_image(), using transparency must first be allowed by calling CRallow_alpha()
-#' surfacep a pointer to a surface, usually an image previously returned by CRload_image()
+#' Set color in color_list to transparent for the image pointed to by image_list and index
+#' image_list is a list with images loaded by CRload_image(), using transparency must first be allowed by calling CRallow_alpha()
+#' index is the index of the image for which transparency will be changed
 #' color_list a list defining a color in RGB, must contain element r, g, b (a is ignored)
 
-CRcolor_to_transparent <- function(surfacep, color_list) {
+CRcolor_to_transparent <- function(image_list, index, color_list) {
   
   if ( !isColorList(color_list) ) {
     stop("Input structure must be \"color list\".")
   } 
   
-  invisible( .Call("SDLcolor_to_transparent", surfacep, color_list$r, color_list$g, color_list$b, PACKAGE = "CREx") )
+  if (!isImageList(image_list)) {
+    stop("Input structure must be \"image list\".")
+  }
+  
+  if (index > length(image_list$imagefile)) {
+    stop("Index is longer than \"image list\".")
+  }
+  
+  invisible( .Call("SDLcolor_to_transparent", image_list$imagefile[[index]], color_list$r, color_list$g, color_list$b, PACKAGE = "CREx") )
   
 }
